b417605a4e350b72ed9bd415ea9131c7e9ecd5b7
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-22 15:45:38 +0100

    add admin Routes

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@ const app = express();
 require("dotenv").config();
 // const cors = require("cors");
 const port = process.env.PORT || 5000;
-require("../../brief-NamX/backend/config/dbConfig");
+require("../../NamX-1/backend/config/dbConfig");
 const bodyParser = require("body-parser");
 
 // app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/admin", require("./routes/admin/adminRoutes"));
 
 // listen to port
 app.listen(port, () => {
