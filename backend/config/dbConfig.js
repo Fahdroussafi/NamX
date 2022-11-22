60c4a495cb2e97f3ac73cc2c3c8778af3597911f
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-22 09:26:20 +0100

    db config

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,11 +1,5 @@
 const mongoose = require("mongoose");
-
-
-mongoose.connect(process.env.mongo_url); //donpmt env
-
-
-
-
+mongoose.connect(process.env.mongo_url);
 const db = mongoose.connection;
 
 db.on("connected", () => {
