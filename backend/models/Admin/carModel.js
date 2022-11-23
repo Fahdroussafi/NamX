c22b6338d391bb5b6e0aaeba9705114c4949e5f7
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-23 10:02:02 +0100

    added car_image

--- backend/models/Admin/carModel.js
+++ backend/models/Admin/carModel.js
@@ -10,6 +10,11 @@ const carSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        car_image: {
+            type: String,
+            required: true,
+            default: "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png",
+        },
         type_id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "types",
