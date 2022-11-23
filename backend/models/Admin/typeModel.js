0fce25938efe880aaf5d95921690a7dcf3d05ad3
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-23 16:18:13 +0100

    Update typeModel.js

--- backend/models/Admin/typeModel.js
+++ backend/models/Admin/typeModel.js
@@ -2,35 +2,20 @@ const mongoose = require("mongoose");
 
 const typeSchema = new mongoose.Schema(
     {
-        max_speed: {
-            type: Number,
-            required: true,
-        },
-        km_range: {
-            type: Number,
-            required: true,
-        },
-        horsepower: {
-            type: Number,
-            required: true,
-        },
-        price: {
-            type: Number,
+        details_id: {
+            type: mongoose.Schema.Types.Array || mongoose.Schema.Types.ObjectId,
+            ref: "details",
             required: true,
         },
         type_name: {
             type: String,
             required: true,
         },
-        stock: {
-            type: Number,
-            required: true,
-        }
     },
     {
         timestamps: true,
     }
 );
 
-module.exports = mongoose.model("type", typeSchema);
+module.exports = mongoose.model("types", typeSchema);
 
