5d566f6afaa36eaee4cfc8d8bccff9f9ec5adb7c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-23 18:38:54 +0100

    added get all detals route

--- backend/routes/admin/detailsRoutes.js
+++ backend/routes/admin/detailsRoutes.js
@@ -3,8 +3,10 @@ const router = express();
 
 const {
     CreateDetails,
+    GetAllDetails
 } = require("../../controllers/admin/detailsController");
 
 router.post("/create-details", CreateDetails);
+router.get("/get-all-details", GetAllDetails);
 
 module.exports = router;
