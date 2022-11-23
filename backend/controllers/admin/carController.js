d2f74f8f2fb892031264bd3fbc237f00530530f8
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-23 18:38:08 +0100

    GetAllCars function

--- backend/controllers/admin/carController.js
+++ backend/controllers/admin/carController.js
@@ -46,7 +46,24 @@ const CreateCar = async (req, res) => {
         });
     }
 }
+const GetAllCars = async (req, res) => {
+    try {
+        const cars = await Car.find();
+        res.status(200).send({
+            success: true,
+            message: "All cars fetched successfully",
+            data: cars,
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+            errorMessage: error.message,
+        });
+    }
+}
 
 module.exports = {
     CreateCar,
+    GetAllCars,
 };
