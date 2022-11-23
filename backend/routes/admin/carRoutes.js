aac040b7308d08188fb6d8ec736a4a8113158d52
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-23 18:39:04 +0100

    Revert "added getallcars routes"
    This reverts commit 71d366c7a34e3dd33bf534bfcbc611eb88ea9db2.

--- backend/routes/admin/carRoutes.js
+++ backend/routes/admin/carRoutes.js
@@ -3,11 +3,9 @@ const router = express();
 
 const {
     CreateCar,
-    GetAllCars
 } = require("../../controllers/admin/carController");
 
 router.post("/create-car/:type_id", CreateCar);
-router.get("/get-all-cars", GetAllCars);
 
 
 module.exports = router;
