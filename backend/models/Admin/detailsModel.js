d5779735bdbfb22cf5c1c8347d572a4716a80d88
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-11-24 14:27:39 +0100

    update detailsModel.js

--- backend/models/Admin/detailsModel.js
+++ backend/models/Admin/detailsModel.js
@@ -9,13 +9,10 @@ const detailsSchema = new mongoose.Schema(
         details_description: {
             type: String,
             required: true,
-<<<<<<< HEAD
-=======
         },
         details_status: {
             type: Boolean,
             default: "true",
->>>>>>> addcar
         }
     },
     {
