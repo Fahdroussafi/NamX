3332285bcb0c4060cc9f255c3400bf2866ae4097
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-27 23:17:06 +0100

    Update CarModel.js

--- api/src/models/CarModel.js
+++ api/src/models/CarModel.js
@@ -1,19 +1,22 @@
 import { model, Schema } from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
 
-const CarSchema = new Schema({
-        name_car: {
-          type: String,
-        },
-      },
-      {
-        timestamps: true,
-      }
+const CarSchema = new Schema(
+  {
+    name_car: {
+      type: String,
+      required: true,
+    },
+    type_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'types',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-
-
-
 CarSchema.plugin(uniqueValidator);
-export const CarModel = model('Car', CarSchema);
-
+export const CarModel = model('cars', CarSchema);
