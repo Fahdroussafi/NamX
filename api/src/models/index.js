9963d641a0858ebe41a0c4f2e496fd08fa933b5c
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-27 23:27:25 +0100

    Update index.js

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -2,3 +2,7 @@ export { CarModel } from './CarModel';
 export { UserModel } from './UserModel';
 export { TypeModel } from './TypeModel';
 export { AdminModel } from './AdminModel';
+export { ColorModel } from './ColorModel';
+export { DetailsModel } from './DetailsModel';
+export { ImageModel } from './ImageModel';
+export { ReservationModel } from './ReservationModel';
