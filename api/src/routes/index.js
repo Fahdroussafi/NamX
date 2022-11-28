cfb1287f7805ef15011abe0c9345424d01f94827
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-28 10:38:53 +0100

    export details routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,3 +2,4 @@ export { CarRoutes } from './CarRoutes';
 export { UsersRoutes } from './UsersRoutes';
 export { AdminRoutes } from './AdminRoutes';
 export { ColorRoutes } from './ColorRoutes';
+export { DetailsRoutes } from './DetailsRoutes';
