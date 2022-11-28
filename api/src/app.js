ba11656298f20bb91e0a8147cfc88a499478d96c
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-11-28 16:41:57 +0100

    update app

--- api/src/app.js
+++ api/src/app.js
@@ -1,50 +1,49 @@
 import 'dotenv/config';
-import { json, static as staticPath, urlencoded } from 'express';
+import {json, urlencoded} from 'express';
 import cors from 'cors';
-import { app, startServer } from './config';
-import { errorHandler, notFound } from './middleware';
-import path from 'path';
+import {app, startServer} from './config';
+import {errorHandler, notFound} from './middleware';
 import {
-  CarRoutes,
-  UsersRoutes,
-  AdminRoutes,
-  ColorRoutes,
-  DetailsRoutes,
-  ReservationRoutes,
-  ImageRoutes,
+    AdminRoutes,
+    CarRoutes,
+    ColorRoutes,
+    DetailsRoutes,
+    ImageRoutes,
+    ReservationRoutes,
+    UsersRoutes,
 } from './routes';
 
 export const init = () => {
-  //global middlewares
+    //global middlewares
 
 
-  app.use(cors({ origin: '*' }));
-  app.use(json());
-  app.use(urlencoded({ extended: true }));
+    app.use(cors({origin: '*'}));
+    app.use(json());
+    app.use(urlencoded({extended: true}));
 
-  //routes
-  app.use('/api/car', CarRoutes);
-  app.use('/api/image', ImageRoutes);
-  app.use('/api/auth', UsersRoutes);
-  app.use('/api/admin', AdminRoutes);
-  app.use('/api/reservation', ReservationRoutes);
-  app.use('/api/color', ColorRoutes);
-  app.use('/api/details', DetailsRoutes);
-  app.get('/', (req, res) => {
-    console.log('health check');
-    res.json({
-      healthCheck: 'system is ready',
+    //routes
+    app.use('/api/car', CarRoutes);
+    app.use('/api/image', ImageRoutes);
+    app.use('/api/auth', UsersRoutes);
+    app.use('/api/admin', AdminRoutes);
+    app.use('/api/reservation', ReservationRoutes);
+    app.use('/api/color', ColorRoutes);
+    app.use('/api/details', DetailsRoutes);
+    app.get('/', (req, res) => {
+        console.log('health check');
+        res.json({
+            healthCheck: 'system is ready',
+        });
     });
-  });
 
-  // 404
-  app.use(notFound);
+    // 404
+    app.use(notFound);
 
-  // Error handler
-  app.use(errorHandler);
+    // Error handler
+    app.use(errorHandler);
 
-  //init db with server
-  startServer();
+    //init db with server
+    startServer();
 };
 
 //initialize server
