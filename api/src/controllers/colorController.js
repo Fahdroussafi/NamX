02c9dadd1dd7ff9bc31a9dc9a9399cad30a4e633
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-28 09:02:29 +0100

    Create colorController.js

---
+++ api/src/controllers/colorController.js
@@ -0,0 +1,32 @@
+import { ColorModel } from '../models';
+
+export const createColor = async (req, res) => {
+  try {
+    const existingColor = await ColorModel.findOne({
+      color_name: req.body.color_name,
+    });
+    if (existingColor) {
+      return res.status(400).send({
+        message: 'Color already exists',
+        success: false,
+        data: null,
+      });
+    }
+    const newColor = new ColorModel({
+      color_name: req.body.color_name,
+      color_code: req.body.color_code,
+    });
+    await newColor.save();
+    res.status(200).send({
+      message: 'Color created successfully',
+      success: true,
+      data: newColor,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+};
