ef58fcdd7973eb0d87d4feb9943d514b11bc5dc2
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-11-28 16:42:55 +0100

    add router put and router delete

--- api/src/routes/ColorRoutes.js
+++ api/src/routes/ColorRoutes.js
@@ -1,17 +1,13 @@
-import { Router } from 'express';
-import {
-  getColors,
-  createColor,
-  updateColor,
-  deleteColor,
-} from '../controllers/colorController';
+import {Router} from 'express';
+import {createColor, deleteColor, getColors, updateColor,} from '../controllers/colorController';
 
 const router = Router();
-const { checkToken } = require('../middleware/adminMiddleware');
+const {checkToken} = require('../middleware/adminMiddleware');
 
 router.get('/get-all-colors', checkToken, getColors);
 router.post('/create-color', checkToken, createColor);
-// router.put('/update-color/:color_id', checkToken, updateColor);
-// router.delete('/delete-color/:color_id', checkToken, deleteColor);
+router.put('/update-color/:color_id', checkToken, updateColor);
+router.delete('/delete-color/:color_id', checkToken, deleteColor);
 
-export { router as ColorRoutes };
+
+export {router as ColorRoutes};
