dcdd75d181e0f74bc69c9f6c21f76a0f9649ee61
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 10:47:42 +0100

    Update typeController.js

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -23,27 +23,55 @@ export const getTypes = async (req, res) => {
 export const createType = async (req, res) => {
   try {
     const { type_name, config } = req.body;
+    // check if the ids in config are in the database and if not, return an error
+
+    for (let i = 0; i < config.details.length; i++) {
+      const detail = await DetailsModel.findById(config.details[i]);
+      if (!detail) {
+        return res.status(404).send({
+          success: false,
+          message: 'Detail not found',
+        });
+      }
+    }
+    for (let i = 0; i < config.image.length; i++) {
+      const image = await ImageModel.findById(config.image[i]);
+      if (!image) {
+        return res.status(404).send({
+          success: false,
+          message: 'Image not found',
+        });
+      }
+    }
+    for (let i = 0; i < config.color.length; i++) {
+      const color = await ColorModel.findById(config.color[i]);
+      if (!color) {
+        return res.status(404).send({
+          success: false,
+          message: 'Color not found',
+        });
+      }
+    }
     const type = await TypeModel.create({
       type_name,
       config,
     });
-    // check if the ids in config are in the database and if not, return an error
-    const details = await DetailsModel.findById({ _id });
-    const image = await ImageModel.findById({ _id });
-    const color = await ColorModel.findById({ _id });
     // if those ids are not the same as the ones in the database, return an error
-    if (!details || !image || !color) {
-      return res.status(400).send({
-        success: false,
-        message: 'Invalid config',
-      });
-    } else if (details && image && color) {
-      res.status(201).send({
-        success: true,
-        message: 'Type created successfully',
-        data: type,
-      });
-    }
+
+    res.status(201).send({
+      success: true,
+      message: 'Type created successfully',
+      data: type,
+      // show the name of the detail, image and color ids
+      datas: {
+        type_name: type.type_name,
+        config: {
+          details: type.config.details_name,
+          image: type.config.image,
+          color: type.config.color_name,
+        },
+      },
+    });
   } catch (error) {
     res.status(500).send({
       success: false,
