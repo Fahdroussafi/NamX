0dd1df7fa6daceff374f62ec201a2c54c888ea1e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 09:27:33 +0100

    Update TypeModel.js

--- api/src/models/TypeModel.js
+++ api/src/models/TypeModel.js
@@ -3,14 +3,47 @@ import uniqueValidator from 'mongoose-unique-validator';
 
 const TypeSchema = new Schema(
   {
-    details_id: {
-      type: Schema.Types.Array || Schema.Types.ObjectId,
-      ref: 'details',
-      required: true,
-    },
     type_name: {
       type: String,
       required: true,
+    },
+    // color_id: {
+    //   type: Schema.Types.Array || Schema.Types.ObjectId || Schema.Types.String,
+    //   ref: 'colors',
+    //   required: true,
+    // },
+    // image_id: {
+    //   type: Schema.Types.Array || Schema.Types.ObjectId || Schema.Types.String,
+    //   ref: 'images',
+    //   required: true,
+    // },
+    // details_id: {
+    //   type: Schema.Types.Array || Schema.Types.ObjectId || Schema.Types.String,
+    //   ref: 'details',
+    //   required: true,
+    // },
+    config: {
+      type: Object,
+      required: true,
+      details: {
+        type: Schema.Types.ObjectId || Schema.Types.String,
+        ref: 'details',
+        required: true,
+        image: {
+          type: Schema.Types.ObjectId || Schema.Types.String,
+          ref: 'images',
+          required: true,
+        },
+        color: {
+          type: Schema.Types.ObjectId || Schema.Types.String,
+          ref: 'colors',
+          required: true,
+        },
+      },
+      // details: {
+      //   type: Object,
+      //   required: true,
+      // },
     },
   },
   {
