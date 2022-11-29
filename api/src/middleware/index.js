bea96830aca6d577fc0773a5311caf0c77e60837
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 21:02:21 +0100

    export nodemailer middleware

--- api/src/middleware/index.js
+++ api/src/middleware/index.js
@@ -4,3 +4,4 @@ export { checkToken } from './adminMiddleware';
 export { checkUserToken } from './authMiddleware';
 export { nodemailer } from './nodemailer';
 export { transporter } from './nodemailer';
+ 
