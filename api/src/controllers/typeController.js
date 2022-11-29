be71e1ee65e3129eda6d47504169aa4b89d29255
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-11-29 13:47:09 +0100

    .

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -1,57 +1,22 @@
 import {ColorModel, DetailsModel, ImageModel, TypeModel} from '../models';
 
+
+//get all types and names of types, details, images, colors
 export const getTypes = async (req, res) => {
     try {
-        const types = await TypeModel.find();
-        res.status(200).send({
-            success: true,
-            message: 'All types fetched successfully',
-            data: types,
-        });
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: 'Internal server error',
-            errorMessage: error.message,
-        });
-    }
-};
+
 
 export const createType = async (req, res) => {
-
     try {
         const {type_name, config} = req.body;
-        const type = await TypeModel.create({
-            type_name,
-            config,
-        });
-        // check if the ids in config are in the database and if not, return an error
-
-        const details = await DetailsModel.findById({details_id});
-        const image = await ImageModel.findById({image_id});
-        const color = await ColorModel.findById({color_id});
-        // if those ids are not the same as the ones in the database, return an error
-        if (!details || !image || !color) {
-            return res.status(400).send({
+        const existingType = await TypeModel.findOne({type_name});
+        if (existingType) {
+            return res.status(409).send({
                 success: false,
-                message: 'Invalid config',
-            });
-        } else if (details && image && color) {
-            res.status(201).send({
-                success: true,
-                message: 'Type created successfully',
-                data: type,
+                message: 'Type name already exists',
             });
         }
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: 'Internal server error',
-            errorMessage: error.message,
-        });
-    }
-    try {
-        const {type_name, config} = req.body;
+
         // check if the ids in config are in the database and if not, return an error
 
         for (let i = 0; i < config.details.length; i++) {
@@ -91,15 +56,6 @@ export const createType = async (req, res) => {
             success: true,
             message: 'Type created successfully',
             data: type,
-            // show the name of the detail, image and color ids
-            datas: {
-                type_name: type.type_name,
-                config: {
-                    details: type.config.details_name,
-                    image: type.config.image,
-                    color: type.config.color_name,
-                },
-            },
         });
     } catch (error) {
         res.status(500).send({
