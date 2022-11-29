ced9145c40cfbd69115f7ead44f2656e30d143bd
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 16:32:32 +0100

    fix logic

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -4,105 +4,87 @@ import { ImageModel } from '../models';
 import { ColorModel } from '../models';
 
 export const getTypes = async (req, res) => {
-    try {
-        const types = await TypeModel.find();
-        const details = await DetailsModel.find();
-        const images = await ImageModel.find();
-        const colors = await ColorModel.find();
-        res.status(200).send({
-            success: true,
-            message: 'All types fetched successfully',
-            data: types,
-            // show the details_name and image and color_code
-            datas: types.map((type) => {
-                return {
-                    type_name: type.type_name,
-                    config: {
-                        details: details.map((detail) => {
-                            return {
-                                details_name: detail.details_name,
-                                details_description: detail.details_description,
-                            };
-                        }),
-
-                        images: images.map((image) => {
-                            return {
-                                image_name: image.name_image,
-                                image: image.image,
-                            };
-                        }),
-
-                        colors: colors.map((color) => {
-                            return {
-                                color_name: color.color_name,
-                                color_code: color.color_code,
-                            };
-                        }),
-                    },
-                };
-            }),
-        });
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: 'Internal server error',
-            errorMessage: error.message,
-        });
-    }
+  try {
+    const types = await TypeModel.find()
+      .populate('details')
+      .populate('image')
+      .populate('color');
+    res.status(200).send({
+      success: true,
+      message: 'All types fetched successfully',
+      data: types,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: 'Internal server error',
+      errorMessage: error.message,
+    });
+  }
 };
 
 export const createType = async (req, res) => {
-    try {
-        const { type_name, config } = req.body;
-        const existingType = await TypeModel.findOne({ type_name });
-        if (existingType) {
-            return res.status(409).send({
-                success: false,
-                message: 'Type name already exists',
-            });
-        }
-
-        for (let i = 0; i < config.details.length; i++) {
-            const detail = await DetailsModel.findById(config.details[i]);
-            if (!detail) {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Detail not found',
-                });
-            }
-        }
-        for (let i = 0; i < config.image.length; i++) {
-            const image = await ImageModel.findById(config.image[i]);
-            if (!image) {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Image not found',
-                });
-            }
-        }
-        for (let i = 0; i < config.color.length; i++) {
-            const color = await ColorModel.findById(config.color[i]);
-            if (!color) {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Color not found',
-                });
-            }
-        }
-        const type = await TypeModel.create({
-            type_name,
-            config,
-        });
-        res.status(201).send({
-            success: true,
-            message: 'Type created successfully',
-            data: type,
-        });
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            message: 'Internal server error',
-            errorMessage: error.message,
-        });
+  try {
+    const { type_name, details, image, color } = req.body;
+    const existingType = await TypeModel.findOne({ type_name });
+    if (existingType) {
+      return res.status(409).send({
+        success: false,
+        message: 'Type name already exists',
+      });
     }
+
+    // check for each details_id entered if it exists in the details collection
+    for (let i = 0; i < details.length; i++) {
+      const existingDetails = await DetailsModel.findById(details[i]);
+      if (!existingDetails) {
+        return res.status(409).send({
+          success: false,
+          message: 'Details id does not exist',
+        });
+      }
+    }
+
+    // check for each image_id entered if it exists in the images collection
+    for (let i = 0; i < image.length; i++) {
+      const existingImage = await ImageModel.findById(image[i]);
+      if (!existingImage) {
+        return res.status(409).send({
+          success: false,
+          message: 'Image id does not exist',
+        });
+      }
+    }
+
+    // check for each color_id entered if it exists in the colors collection
+
+    for (let i = 0; i < color.length; i++) {
+      const existingColor = await ColorModel.findById(color[i]);
+      if (!existingColor) {
+        return res.status(409).send({
+          success: false,
+          message: 'Color id does not exist',
+        });
+      }
+    }
+
+    const type = await TypeModel.create({
+      type_name,
+      details,
+      image,
+      color,
+    });
+
+    res.status(201).send({
+      success: true,
+      message: 'Type created successfully',
+      data: type,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: 'Internal server error',
+      errorMessage: error.message,
+    });
+  }
 };
--- api/src/models/TypeModel.js
+++ api/src/models/TypeModel.js
@@ -2,34 +2,30 @@ import { model, Schema } from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
 
 const TypeSchema = new Schema(
-    {
-        type_name: {
-            type: String,
-            required: true,
-        },
-        config: {
-            type: Object,
-            required: true,
-            details: {
-                type: [Schema.Types.ObjectId],
-                ref: 'details',
-                required: true,
-                image: {
-                    type: [Schema.Types.ObjectId],
-                    ref: 'images',
-                    required: true,
-                },
-                color: {
-                    type: [Schema.Types.ObjectId],
-                    ref: 'colors',
-                    required: true,
-                },
-            },
-        },
+  {
+    type_name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    details: {
+      type: [Schema.Types.ObjectId],
+      ref: 'details',
+      required: true,
+    },
+    image: {
+      type: [Schema.Types.ObjectId],
+      ref: 'images',
+      required: true,
+    },
+    color: {
+      type: [Schema.Types.ObjectId],
+      ref: 'colors',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 TypeSchema.plugin(uniqueValidator);
