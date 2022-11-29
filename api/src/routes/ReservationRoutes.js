29a8608b4a93144546beae29517dd30a1cd78390
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 09:27:22 +0100

    Update ReservationRoutes.js

--- api/src/routes/ReservationRoutes.js
+++ api/src/routes/ReservationRoutes.js
@@ -1,20 +1,36 @@
 import { Router } from 'express';
 import {
-    createReservation,
-    GetAllReservations,
-    getReservationsByUserId,
-    UpdateReservation,
-    CancelReservation,
+  createReservation,
+  GetAllReservations,
+  getReservationsByUserId,
+  UpdateReservation,
+  CancelReservation,
 } from '../controllers/reservationController';
-import {checkUserToken} from "../middleware";
+import { checkUserToken } from '../middleware';
 
 const router = Router();
 const { checkToken } = require('../middleware/adminMiddleware');
 
-router.post('/create-reservation/:user_id/:car_id',checkUserToken, createReservation);
+router.post(
+  '/create-reservation/:user_id/:car_id',
+  checkUserToken,
+  createReservation
+);
 router.get('/get-all-reservations', checkToken, GetAllReservations);
-router.get('/get-reservations-by-user-id/:user_id',checkUserToken, getReservationsByUserId);
-router.put('/update-reservation/:user_id/:reservation_id',checkUserToken, UpdateReservation);
-router.delete('/cancel-reservation/:user_id/:reservation_id',checkUserToken, CancelReservation);
+router.get(
+  '/get-reservations-by-user-id/:user_id',
+  checkUserToken,
+  getReservationsByUserId
+);
+router.put(
+  '/update-reservation/:user_id/:reservation_id',
+  checkUserToken,
+  UpdateReservation
+);
+router.delete(
+  '/cancel-reservation/:user_id/:reservation_id',
+  checkUserToken,
+  CancelReservation
+);
 
 export { router as ReservationRoutes };
