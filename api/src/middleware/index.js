0e849b6acb872654d8a8a42468f45ad0ffc69e61
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-11-29 21:00:31 +0100

    export nodemailer and transporter

--- api/src/middleware/index.js
+++ api/src/middleware/index.js
@@ -2,3 +2,5 @@ export { notFound } from './notFound';
 export { errorHandler } from './errorHandler';
 export { checkToken } from './adminMiddleware';
 export { checkUserToken } from './authMiddleware';
+export { nodemailer } from './nodemailer';
+export { transporter } from './nodemailer';
