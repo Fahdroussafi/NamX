85c719bc880a718e920d13e92eb9f53476f048ff
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-02 15:38:50 +0100

    update App

--- namx/src/App.jsx
+++ namx/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+import About from './components/About';
+import Developers from './components/Developers';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Subscribe from './components/Subscribe';
+import './index.css';
+import './style.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Developers />
+        <Subscribe />
+        <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
