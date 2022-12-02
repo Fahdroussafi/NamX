6482d0b0d3d5b0a670b208f81e4665f6a710629e
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-02 15:42:42 +0100

    add About & Footer & Navbar

---
+++ namx/src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { AiOutlineLogin } from "react-icons/ai";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    
+    <div>
+      <div class="fixed w-full">
+        <div className="flex justify-between items-center h-full backdrop-blur-sm">
+          <div className="flex mt-8 justify-center w-screen">
+            <img className="max-w-[250px]" src={logo} alt="/" />
+          </div>
+          <div className="hidden md:flex text-white mr-5">
+          <ul className="flex text-white items-center">
+              <li>Store</li>
+              <li>Contact</li>
+            </ul>
+            <button className="ml-4">
+              <AiOutlineLogin className="text-2xl" size={20} />
+            </button>
+          </div>
+          {/* Hamburger menu */}
+          {/* <div onClick={handleNav} className="block md:hidden">
+          {nav ? (
+            <AiOutlineClose size={30} className="text-white" />
+            ) : (
+              <AiOutlineMenu size={30} className="text-white" />
+              )}
+        </div> */}
+
+          {/* Mobile Menu */}
+          {/* <div
+          className={
+            nav
+            ? "w-full bg-black bg-opacity-40 text-white absolute top-[90px] left-0 flex justify-center text-center"
+            : "absolute left-[-100%]"
+          }
+          >
+          <ul>
+          <li className="text-2xl">Platform</li>
+          <li className="text-2xl">Developers</li>
+            <li className="text-2xl">Community</li>
+            <li className="text-2xl">About</li>
+            <button className="m-8">Login</button>
+            </ul>
+          </div> */}
+        </div>
+          <div className="w-full" >
+          <input type="checkbox" class="toggler" />
+          <div class="hamburger">
+            <div></div>
+          </div>
+          <div class="menu">
+            <div>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
