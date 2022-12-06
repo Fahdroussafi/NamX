d3c876a35486e7bf072d9d4a2fd2afc117246572
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-06 13:33:49 +0100

    Create App.js

---
+++ React-Dashboard/src/App.js
@@ -0,0 +1,105 @@
+import React, { lazy, Suspense } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
+
+import Loader from "./helpers/loader";
+
+const Orders = lazy(() => import("./pages/Orders"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Cars = lazy(() => import("./pages/Cars"));
+const Types = lazy(() => import("./pages/Types"));
+const Details = lazy(() => import("./pages/Details"));
+const Colors = lazy(() => import("./pages/Colors"));
+const Images = lazy(() => import("./pages/Images"));
+const Login = lazy(() => import("./pages/Login"));
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path="/orders"
+              element={
+                <ProtectedRoute>
+                  <Orders />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/images"
+              element={
+                <ProtectedRoute>
+                  <Images />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/colors"
+              element={
+                <ProtectedRoute>
+                  <Colors />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/cars"
+              element={
+                <ProtectedRoute>
+                  <Cars />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/types"
+              element={
+                <ProtectedRoute>
+                  <Types />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/details"
+              element={
+                <ProtectedRoute>
+                  <Details />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<h1>404</h1>} />
+
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
