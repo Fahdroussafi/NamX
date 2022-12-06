d08aa3a02b55a462a06bcc649c27dc27a1d56557
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-12-06 09:58:10 +0100

    build a scene with reactthreefiber

--- namx/src/App.jsx
+++ namx/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import { AxesHelper } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-function App() {
-  const [count, setCount] = useState(0)
+extend({ OrbitControls });
+
+const Orbit = () => {
+  const { camera, gl } = useThree();
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} />;
+};
+
+const Box = (props) => {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <mesh ref={ref} {...props}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="hotpink" />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
+        <Box position={[1, 1, 0]} />
+        <Orbit />
+        <axesHelper args={[5]} />
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
