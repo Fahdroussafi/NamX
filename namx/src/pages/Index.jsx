3dd7e2949f4daf580049cb215aa9ae3d1eb273a8
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-07 10:03:00 +0100

    add home and login and register pages

---
+++ namx/src/pages/Index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import About from "../components/About";
+import Developers from "../components/Developers";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Subscribe from "../components/Subscribe";
+
+function Index() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Developers />
+      <Subscribe />
+      <Footer />
+    </div>
+  );
+}
+
+export default Index;
