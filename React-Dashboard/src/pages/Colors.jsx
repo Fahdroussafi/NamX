4e9abc3f6de986f050c542bbfd35baa2e3aa983b
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-08 16:04:52 +0100

    Update Colors.jsx

--- React-Dashboard/src/pages/Colors.jsx
+++ React-Dashboard/src/pages/Colors.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { axiosInstance } from "../helpers/axiosInstance";
 import { message, Table } from "antd";
 import ColorsForm from "../components/Forms/ColorsForm";
+import DoneIcon from "../assets/icons/done.svg";
 
 function Colors() {
   const [colors, setColors] = useState([]);
@@ -45,6 +46,7 @@ function Colors() {
             height: "50px",
             borderRadius: "50%",
             backgroundColor: color_code,
+            border: "0.5px #000 solid",
           }}
         ></div>
       ),
@@ -55,21 +57,22 @@ function Colors() {
       dataIndex: "action",
       render: (text, record) => (
         <div className="flex gap-2">
-          <button
-            className="underline text-base text-green-500 cursor-pointer hover:text-green-700"
-            onClick={() => {
-              // setSelectedCar(record);
-            }}
-          >
-            View
-          </button>
-          <button
-            className="underline text-base text-red-500 cursor-pointer hover:text-red-700"
-            onClick={() => {
-              // CancelBooking();
-            }}
-          >
-            Cancel
+          <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Delete
           </button>
         </div>
       ),
@@ -103,6 +106,7 @@ function Colors() {
         dataSource={colors}
         columns={columns}
         pagination={{ pageSize: 5 }}
+        loading={colors.length === 0}
       />
       {showColorForm && (
         <ColorsForm
