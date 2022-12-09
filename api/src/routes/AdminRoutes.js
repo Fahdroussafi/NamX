a9f5ba7fd410a1e92268a04956de3992c3c02548
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-09 10:11:16 +0100

    get all users route

--- api/src/routes/AdminRoutes.js
+++ api/src/routes/AdminRoutes.js
@@ -3,12 +3,14 @@ import {
   registerAdmin,
   loginAdmin,
   getAdminById,
+  getAllUsers,
 } from '../controllers/adminController';
 
 const router = Router();
 
+router.get('/get-all-users', getAllUsers);
 router.post('/login-admin', loginAdmin);
 router.post('/create-admin', registerAdmin);
 router.get('/:admin_id', getAdminById);
-
+// router.get('/get-all-users', getAllUsers);
 export { router as AdminRoutes };
