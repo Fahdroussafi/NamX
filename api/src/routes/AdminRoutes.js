ec501e7b05875d215918d90ab7e5a20c5125c475
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-09 19:40:02 +0100

    add route get all users

--- api/src/routes/AdminRoutes.js
+++ api/src/routes/AdminRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { registerAdmin, loginAdmin } from '../controllers/adminController';
+import { registerAdmin, loginAdmin, getAllUsers } from '../controllers/adminController';
 
 const router = Router();
 
 router.post('/login-admin', loginAdmin);
 router.post('/create-admin', registerAdmin);
+router.get('/get-all-users', getAllUsers);
 
 export { router as AdminRoutes };
