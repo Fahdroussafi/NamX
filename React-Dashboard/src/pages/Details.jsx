adff4755298a368c0a5774bc6e1fcd349ff38395
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-15 16:47:55 +0100

    Update Details.jsx

--- React-Dashboard/src/pages/Details.jsx
+++ React-Dashboard/src/pages/Details.jsx
@@ -35,16 +35,28 @@ function Details() {
     {
       title: "Details Name",
       dataIndex: "details",
-      render: (details) =>
-        details.map((detail) => detail.details_name).join(", "),
-      key: "details",
+      // render each details name in a new line using map
+      render: (details) => (
+        <div>
+          {details.map((detail, key) => (
+            <div key={key}>{detail.details_name}</div>
+          ))}
+        </div>
+      ),
+
+      key: "details_name",
     },
 
     {
       title: "Details Description",
       dataIndex: "details",
-      render: (details) =>
-        details.map((detail) => detail.details_description).join(", "),
+      render: (details) => (
+        <div>
+          {details.map((detail, key) => (
+            <div key={key}>{detail.details_description}</div>
+          ))}
+        </div>
+      ),
       key: "details_description",
     },
     {
@@ -53,20 +65,26 @@ function Details() {
       render: (text, record) => (
         <div className="flex gap-2">
           <button
-            className="underline text-base text-green-500 cursor-pointer hover:text-green-700"
             onClick={() => {
-              // setSelectedCar(record);
+              // deleteColor(record._id);
             }}
+            className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md"
           >
-            View
-          </button>
-          <button
-            className="underline text-base text-red-500 cursor-pointer hover:text-red-700"
-            onClick={() => {
-              // CancelBooking();
-            }}
-          >
-            Cancel
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Delete
           </button>
         </div>
       ),
