07a3165300497947601752ac51d175fa059033fa
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-15 16:51:34 +0100

    update about card

--- namx/src/components/AboutCard.jsx
+++ namx/src/components/AboutCard.jsx
@@ -4,7 +4,7 @@ const AboutCard = (props) => {
   return (
     <div className='flex flex-col border text-left rounded-2xl py-12 px-8'>
     <div>
-      <div className='bg-[#00d8ff] inline-flex p-2 rounded-full'>
+      <div className='bg-[#000] inline-flex p-2 rounded-full'>
         {props.icon}
       </div>
       <h3 className='text-xl font-bold py-4'>{props.heading}</h3>
