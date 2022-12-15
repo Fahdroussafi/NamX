1aa6470329f7b7a95488172a382d2e147b1a6007
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-15 16:47:32 +0100

    Update ImagesForm.jsx

--- React-Dashboard/src/components/Forms/ImagesForm.jsx
+++ React-Dashboard/src/components/Forms/ImagesForm.jsx
@@ -1,137 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { Modal, Row, Form, Col, message, Upload, Button } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import FileInput from "../FileInput";
 import { axiosInstance } from "../../helpers/axiosInstance";
+import styles from "./styles.module.css";
+import { message } from "antd";
 
-function ImagesForm({
-  showImagesForm,
-  setShowImagesForm,
-  type = "add",
-  getData,
-  selectedImages,
-  setSelectedImages,
-}) {
-  const onFinish = async (values) => {
-    try {
-      const response = await axiosInstance.post(
-        "/api/image/create-image",
-        values
-      );
-      if (type === "add") {
-        if (response.data.success) {
-          setShowImagesForm(false);
-          getData();
-        } else {
-          message.error("Car name or type already exists");
+const ImagesForm = () => {
+  const [data, setData] = useState({
+    name: "",
+    img: "",
+  });
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleInputState = (name, value) => {
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (data.name === "" || data.img === "") {
+      message.error("Please fill all the fields");
+    } else {
+      try {
+        const url = process.env.REACT_APP_API_URL + "/image/create-image";
+        const { data: res } = await axiosInstance.post(url, data);
+        if (res.success) {
+          message.success(res.message);
+          window.location.reload();
         }
-      } else {
-        response = await axiosInstance.put(
-          `/api/car/${selectedImages._id}`,
-          values
-        );
+      } catch (error) {
+        message.error(error.message);
       }
-      if (response.data.success) {
-        message.success(response.data.message);
-      } else {
-        message.error("Car name or type already exists");
-      }
-      getData();
-      setShowImagesForm(false);
-      setSelectedImages(null);
-    } catch (error) {
-      message.error("Car name or type already exists");
-      console.log(error);
     }
   };
-  // const normFile = (e) => {
-  //   console.log("Upload event:", e);
-
-  //   if (Array.isArray(e)) {
-  //     return e;
-  //   }
-  // };
 
   return (
-    <Modal
-      width={800}
-      title={type === "add" ? "Add Image" : "Update Image"}
-      open={showImagesForm}
-      onCancel={() => {
-        setSelectedImages(null);
-        setShowImagesForm(false);
-      }}
-      footer={false}
-    >
-      <Form
-        layout="vertical"
-        onFinish={onFinish}
-        initialValues={selectedImages}
-      >
-        <Row gutter={[10, 10]}>
-          <Col lg={24} xs={24}>
-            <Form.Item
-              label="Image Name"
-              name="name_image"
-              rules={[
-                {
-                  required: type === "add" ? true : true,
-                  message:
-                    type === "add"
-                      ? "Please enter image name"
-                      : "Please enter image name",
-                },
-              ]}
-            >
-              <input
-                type="text"
-                className="block border border-blue-500 w-full p-3 rounded-lg mb-4"
-              />
-            </Form.Item>
-          </Col>
-          <Col lg={12} xs={24}>
-            <Form.Item
-              label="Image"
-              name="image"
-              valuePropName="fileList"
-              // getValueFromEvent={normFile}
-              rules={[
-                {
-                  required: type === "add" ? true : true,
-                  message:
-                    type === "add"
-                      ? "Please enter image"
-                      : "Please enter image",
-                },
-              ]}
-            >
-              <Upload
-                name="image"
-               action={`${process.env.REACT_APP_API_URL}/api/image/upload-image`}
-                listType="picture"
-              >
-                <Button icon={<UploadOutlined />}>Click to upload</Button>
-              </Upload>
-            </Form.Item>
-          </Col>
-        </Row>
-        <div className="flex justify-end">
-          <button
-            type="submit"
-            className="relative inline-flex items-center justify-start
-                px-10 py-3 overflow-hidden font-bold rounded-full
-                group"
-          >
-            <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-            <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-blue-600 opacity-100 group-hover:-translate-x-8"></span>
-            <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-white">
-              Save
-            </span>
-            <span className="absolute inset-0 border-2 border-blue-600 rounded-full"></span>
-          </button>
-        </div>
-      </Form>
-    </Modal>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h1 className={styles.heading}>Images Form</h1>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Image Name"
+          name="name"
+          onChange={handleChange}
+          value={data.name}
+        />
+        <FileInput
+          name="img"
+          label="Choose Image"
+          handleInputState={handleInputState}
+          type="image"
+          value={data.img}
+        />
+        <button type="submit" className={styles.submit_btn}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
-}
+};
 
 export default ImagesForm;
