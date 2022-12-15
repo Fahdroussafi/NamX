51195c4d7a00620b2b7fa9d5296f9baa53a56dac
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-15 16:47:43 +0100

    Update Cars.jsx

--- React-Dashboard/src/pages/Cars.jsx
+++ React-Dashboard/src/pages/Cars.jsx
@@ -13,14 +13,11 @@ function Cars() {
     try {
       const response = await axiosInstance.get("/api/car/get-all-cars", {});
       if (response.data.success) {
-        // console.log(response.data.data);
 
         const mappedData = response.data.data.map((car) => {
           return {
             ...car,
             key: car._id,
-            key: car.name_car,
-            key: car.type_id.type_name,
           };
         });
         setCars(mappedData);
@@ -49,21 +46,22 @@ function Cars() {
       dataIndex: "action",
       render: (text, record) => (
         <div className="flex gap-2">
-          <button
-            className="underline text-base text-green-500 cursor-pointer hover:text-green-700"
-            onClick={() => {
-              // setSelectedCar(record);
-            }}
-          >
-            View
-          </button>
-          <button
-            className="underline text-base text-red-500 cursor-pointer hover:text-red-700"
-            onClick={() => {
-              // CancelBooking();
-            }}
-          >
-            Cancel
+          <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Delete
           </button>
         </div>
       ),
@@ -97,7 +95,7 @@ function Cars() {
         dataSource={cars}
         columns={columns}
         pagination={{ pageSize: 5 }}
-        loading={cars.length === 0}
+        // loading={cars.length === 0}
       />
       {showCarForm && (
         <CarForm
