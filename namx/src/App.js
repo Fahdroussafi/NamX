e1e0a6cbcf4784c012bdb755ffb54f5c47460bbd
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-15 16:54:58 +0100

    update app

--- namx/src/App.js
+++ namx/src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Index from "./pages/Index";
+import Store from "./pages/Store";
+import Contact from "./pages/Contact";
+import ShowCar from "./pages/ShowCar";
+
 import "./style.css";
 import "./index.css";
 
@@ -16,6 +20,9 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/showcar" element={<ShowCar />} />
         </Routes>
       </BrowserRouter>
     </div>
