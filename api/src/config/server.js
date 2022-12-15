6f88b16942accd855a92e7a9cc8d935caff4b0e4
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-12-15 16:59:25 +0100

    Update server.js

--- api/src/config/server.js
+++ api/src/config/server.js
@@ -1,21 +1,20 @@
-import express from "express"
-import mongoose from "mongoose";
-export const app = express()
+import express from 'express';
+import mongoose from 'mongoose';
+export const app = express();
 
-const { PORT, MONGO_USER, MONGO_PASS, MONGO_DBNAME } = process.env
-const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.xde6xue.mongodb.net/${MONGO_DBNAME}`
+const { PORT, MONGO_USER, MONGO_PASS, MONGO_DBNAME } = process.env;
+const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.xde6xue.mongodb.net/${MONGO_DBNAME}`;
 
-export const startServer = () => mongoose
+export const startServer = () =>
+  mongoose
     .connect(MONGO_URI)
     .then(() => {
-        app.listen(PORT || 5000);
-        console.log(
-            `🚀 Server ready at http://localhost:${PORT} 🚀`
-        );
+      app.listen(PORT || 5000);
+      console.log(`🚀 Server ready at http://localhost:${PORT} 🚀`);
     })
     .catch((err) => {
-        console.error(err);
+      console.error(err);
     });
 
 // mongoose strict mode
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
