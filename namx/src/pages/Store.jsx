0a80a10f8de986cc2a79079b6221575f32d74c2b
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-19 16:46:19 +0100

    complet merge

--- namx/src/pages/Store.jsx
+++ namx/src/pages/Store.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Photo from "../assets/NAMX-01.jpg";
+import { useState } from "react";
+import { axiosInstance } from "../../../React-Dashboard/src/helpers/axiosInstance";
 
-const products = [
-  {
-    id: 1,
-    name: "Machined Pen",
-    color: "Black",
-    price: "$35",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-02-product-01.jpg",
-    imageAlt:
-      "Black machined steel pen with hexagonal grip and small white logo at top.",
-    availableColors: [
-      { name: "Black", colorBg: "#111827" },
-      { name: "Brass", colorBg: "#FDE68A" },
-      { name: "Chrome", colorBg: "#E5E7EB" },
-    ],
-  },
-  // More products...
-];
 
-function Store() {
+
+function Cars() {
+  const [cars, setCars] = useState([]);
+
+  const getCars = async () => {
+    try {
+      const response = await axiosInstance.get("/api/car/get-all-cars", {});
+      if (response.data.success) {
+        const mappedData = response.data.data.map((car) => {
+          return {
+            ...car,
+            key: car._id,
+          };
+        });
+        setCars(mappedData);
+      } else {
+        ("Something went wrong");
+      }
+    } catch (error) {
+      ("Something went wrong");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
+
   return (
     <>
       <div className="bg-black h-1/2">
@@ -52,28 +63,25 @@ function Store() {
                 role="list"
                 className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-4 lg:gap-x-8"
               >
-                {products.map((product) => (
+                {cars.map((car) => (
                   <li
-                    key={product.id}
+                    key={car.id}
                     className="w-64 inline-flex flex-col text-center lg:w-auto"
                   >
                     <div className="group relative">
                       <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
                         <img
-                          src={product.imageSrc}
-                          alt={product.imageAlt}
+                          src={car.imageSrc}
+                          alt={car.imageAlt}
                           className="w-full h-full object-center object-cover group-hover:opacity-75"
                         />
                       </div>
                       <div className="mt-6">
-                        <p className="text-sm text-gray-500">{product.color}</p>
                         <h3 className="mt-1 font-semibold text-gray-900">
-                          <a href={product.href}>
                             <span className="absolute inset-0" />
-                            {product.name}
-                          </a>
+                            {car.name}
                         </h3>
-                        <p className="mt-1 text-gray-900">{product.price}</p>
+                        {/* <p className="mt-1 text-gray-900">{product.price}</p> */}
                       </div>
                     </div>
 
@@ -82,7 +90,7 @@ function Store() {
                       role="list"
                       className="mt-auto pt-6 flex items-center justify-center space-x-3"
                     >
-                      {product.availableColors.map((color) => (
+                      {/* {product.availableColors.map((color) => (
                         <li
                           key={color.name}
                           className="w-4 h-4 rounded-full border border-black border-opacity-10"
@@ -90,7 +98,7 @@ function Store() {
                         >
                           <span className="sr-only">{color.name}</span>
                         </li>
-                      ))}
+                      ))} */}
                     </ul>
                   </li>
                 ))}
@@ -112,4 +120,4 @@ function Store() {
   );
 }
 
-export default Store;
+export default Cars;
